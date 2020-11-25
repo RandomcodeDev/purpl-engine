8702d07b0a7744aa3aebd8b58a8029172990610b
MobSlicer152 <brambleclaw1414@gmail.com>
2020-11-25 18:40:45 -0500

    Fixed a typo and made the demo the default startup project

--- include/purpl/vulkan/buffer.h
+++ include/purpl/vulkan/buffer.h
@@ -8,6 +8,8 @@
 #include <string.h>
 #include <errno.h>
 
+#include <cglm/call.h>
+
 #include <vulkan/vulkan.h>
 
 #include "purpl/macro.h"
@@ -16,9 +18,23 @@
 
 namespace purpl
 {
+/* A structure to hold information about a vertex */
+struct vert_info {
+	vec4 pos;
+	vec4 color;
+};
+
 /*
- * Gets the 
+ * Gets the binding description for a vertex input buffer.
+ * Defined in buffer.cc
  */
+VkVertexInputBindingDescription get_vert_input_binding_desc(struct vert_info vert);
+
+/*
+ * Gets the attribute descriptions for a vertex input buffer.
+ * Defined in buffer.cc
+ */
+VkVertexInputAttributeDescription *get_vert_input_attrib_descs(struct vert_info vert);
 }
 
 #endif /* !PURPL_VULKAN_BUFFER_H */
--- src/vulkan/buffer.cc
+++ src/vulkan/buffer.cc
@@ -1,4 +1,18 @@
 #include "purpl/vulkan/buffer.h"
 using namespace purpl;
 
+VkVertexInputBindingDescription purpl::get_vert_input_binding_desc(struct vert_info vert)
+{
+	VkVertexInputBindingDescription binding_description = {};
+	binding_description.binding = 0;
+	binding_description.stride = sizeof(vert);
+	binding_description.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
 
+	return binding_description;
+}
+
+VkVertexInputAttributeDescription *purpl::get_vert_input_attrib_desc(struct vert_info vert)
+{
+	VkVertexInputAttributeDescription *attrib_descs = {};
+
+}

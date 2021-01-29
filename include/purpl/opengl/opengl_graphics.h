ba75ce7685eaaa495e651d428a221b2aaf8c7e4c
MobSlicer152 <brambleclaw1414@gmail.com>
2021-01-29 10:17:49 -0500

    General refactoring

---
+++ include/purpl/opengl/opengl_graphics.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef PURPL_OPENGL_OPENGL_H
+#define PURPL_OPENGL_OPENGL_H 1
+
+#include "purpl/x11/opengl.h"
+
+namespace purpl {
+typedef x11_opengl_inst opengl_inst;
+}
+
+#endif /* !PURPL_OPENGL_OPENGL_H */

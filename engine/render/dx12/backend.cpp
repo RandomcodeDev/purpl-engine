27f5f47d8089b58d1a77a431fff08a8843ba9d17
MobSlicer152 <brambleclaw1414@gmail.com>
2024-01-31 01:48:33 -0500

    Add blank DX12 backend

---
+++ engine/render/dx12/backend.cpp
@@ -0,0 +1,23 @@
+/*++
+
+Copyright (c) 2024 Randomcode Developers
+
+Module Name:
+
+    backend.c
+
+Abstract:
+
+    This module implements the DirectX 12 backend.
+
+--*/
+
+#include "dx12.h"
+
+EXTERN_C
+VOID
+Dx12InitializeBackend(
+    _Out_ PRENDER_BACKEND Backend
+    )
+{
+}
---
+++ engine/render/dx12/dx12.h
@@ -0,0 +1,29 @@
+/*++
+
+Copyright (c) 2024 Randomcode Developers
+
+Module Name:
+
+    dx12.h
+
+Abstract:
+
+    This module contains definitions used by the DirectX 12 backend.
+
+--*/
+
+#pragma once
+
+#include "purpl/purpl.h"
+
+BEGIN_EXTERN_C
+#include "common/common.h"
+#include "common/log.h"
+
+#include "engine/render/render.h"
+
+#include "platform/video.h"
+
+#include "util/mesh.h"
+#include "util/texture.h"
+END_EXTERN_C

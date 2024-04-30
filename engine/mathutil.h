226b860c5a90d4b928dc9e9b6a2a322709039427
MobSlicer152 <brambleclaw1414@gmail.com>
2024-04-30 15:04:30 -0400

    Make mathutil.h have intrinsics, so it's easier to use in render

--- engine/mathutil.h
+++ engine/mathutil.h
@@ -14,5 +14,15 @@
 /// @param[in] Rotation The rotation to apply (default is 0, 0, 0, 0)
 /// @param[in] Scale The scaling to apply (default is 1, 1, 1)
 /// @param[out] Transform The transformation matrix to fill
-extern VOID MthCreateTransformMatrix(_In_opt_ vec3 Position, _In_opt_ vec4 Rotation, _In_opt_ vec3 Scale,
-                                     _Out_ mat4 Transform);
+static inline VOID MthCreateTransformMatrix(_In_opt_ vec3 Position, _In_opt_ vec4 Rotation, _In_opt_ vec3 Scale,
+                                            _Out_ mat4 Transform)
+{
+    vec3 DefaultPosition = {0.0f, 0.0f, 0.0f};
+    vec4 DefaultRotation = {0.0f, 0.0f, 0.0f, 0.0f};
+    vec3 DefaultScale = {1.0f, 1.0f, 1.0f};
+
+    glm_mat4_identity(Transform);
+    glm_translate(Transform, Position ? Position : DefaultPosition);
+    glm_rotate(Transform, glm_rad(Rotation ? Rotation[3] : DefaultRotation[3]), Rotation ? Rotation : DefaultRotation);
+    glm_scale(Transform, Scale ? Scale : DefaultScale);
+}

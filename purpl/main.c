bd0d54764245546081faee5e9202e58a83a09fb9
MobSlicer152 <brambleclaw1414@gmail.com>
2024-05-10 12:10:03 -0400

    Make renderer demo better

--- purpl/main.c
+++ purpl/main.c
@@ -11,6 +11,20 @@
 
 #include "engine/engine.h"
 
+VOID Spin(_In_ ecs_iter_t *Iterator)
+{
+    PROTATION Rotation = ecs_field(Iterator, ROTATION, 2);
+
+    for (UINT32 i = 0; i < Iterator->count; i++)
+    {
+        // Only on Y
+        if (Rotation[i].Value[1] >= 1.0)
+        {
+            Rotation[i].Value[3] += 20 * Iterator->delta_time;
+        }
+    }
+}
+
 // #define PURPL_TESTING_IN_MAIN
 
 INT PurplMain(_In_ PCHAR *Arguments, _In_ UINT ArgumentCount)
@@ -25,6 +39,8 @@ INT PurplMain(_In_ PCHAR *Arguments, _In_ UINT ArgumentCount)
     EngDefineVariables();
     CmnInitialize(Arguments, ArgumentCount);
     EngInitialize();
+
+    ECS_SYSTEM(EcsGetWorld(), Spin, EcsOnUpdate, MODEL, ROTATION);
 
     ecs_entity_t CameraEntity = EcsCreateEntity("camera");
     DOUBLE Aspect = (DOUBLE)RdrGetWidth() / (DOUBLE)RdrGetHeight();
@@ -43,14 +59,28 @@ INT PurplMain(_In_ PCHAR *Arguments, _In_ UINT ArgumentCount)
     RENDER_OBJECT_DATA TestObject = {0};
     RdrInitializeObject("test", &TestObject, &TestModel);
     ecs_set_ptr(EcsGetWorld(), TestEntity, RENDER_OBJECT_DATA, &TestObject);
-
-    ecs_set(EcsGetWorld(), TestEntity, POSITION, {{0.0, 0.0, 0.0}});
-    ecs_set(EcsGetWorld(), TestEntity, ROTATION, {{0.0, 1.0, 0.0, -180.0}});
+    ecs_set(EcsGetWorld(), TestEntity, POSITION, {{-1.0, 0.0, 0.0}});
+    ecs_set(EcsGetWorld(), TestEntity, ROTATION, {{0.0, 1.0, 0.0, 270.0}});
     ecs_set(EcsGetWorld(), TestEntity, SCALE, {{1.0, 1.0, 1.0}});
 
+    ecs_entity_t Test2Entity = EcsCreateEntity("test2");
+    RENDER_HANDLE Test2Texture = RdrLoadTexture("chief_alt.ptex");
+    MATERIAL Test2Material = {0};
+    RdrCreateMaterial(&Test2Material, Test2Texture, "main");
+    MODEL Test2Model = {0};
+    RdrLoadModel(&Test2Model, "chief.pmdl", &Test2Material);
+    ecs_set_ptr(EcsGetWorld(), Test2Entity, MODEL, &Test2Model);
+    RENDER_OBJECT_DATA Test2Object = {0};
+    RdrInitializeObject("test2", &Test2Object, &Test2Model);
+    ecs_set_ptr(EcsGetWorld(), Test2Entity, RENDER_OBJECT_DATA, &Test2Object);
+    ecs_set(EcsGetWorld(), Test2Entity, POSITION, {{1.0, 0.0, 0.0}});
+    ecs_set(EcsGetWorld(), Test2Entity, ROTATION, {{0.0, 1.0, 0.0, -270.0}});
+    ecs_set(EcsGetWorld(), Test2Entity, SCALE, {{1.0, 1.0, 1.0}});
+
     ecs_entity_t GroundEntity = EcsCreateEntity("ground");
+    RENDER_HANDLE GroundTexture = RdrLoadTexture("ground.ptex");
     MATERIAL GroundMaterial = {0};
-    RdrCreateMaterial(&GroundMaterial, TestTexture, "main");
+    RdrCreateMaterial(&GroundMaterial, GroundTexture, "main");
     MODEL GroundModel = {0};
     RdrLoadModel(&GroundModel, "ground.pmdl", &GroundMaterial);
     ecs_set_ptr(EcsGetWorld(), GroundEntity, MODEL, &GroundModel);
@@ -66,14 +96,20 @@ INT PurplMain(_In_ PCHAR *Arguments, _In_ UINT ArgumentCount)
 
     EngMainLoop();
 
-    RdrDestroyObject(ecs_get(EcsGetWorld(), TestEntity, RENDER_OBJECT_DATA));
-    RdrDestroyModel(ecs_get(EcsGetWorld(), TestEntity, MODEL));
-    RdrDestroyMaterial(&TestMaterial);
-
     RdrDestroyObject(ecs_get(EcsGetWorld(), GroundEntity, RENDER_OBJECT_DATA));
     RdrDestroyModel(ecs_get(EcsGetWorld(), GroundEntity, MODEL));
     RdrDestroyMaterial(&GroundMaterial);
 
+    RdrDestroyObject(ecs_get(EcsGetWorld(), Test2Entity, RENDER_OBJECT_DATA));
+    RdrDestroyModel(ecs_get(EcsGetWorld(), Test2Entity, MODEL));
+    RdrDestroyMaterial(&Test2Material);
+
+    RdrDestroyObject(ecs_get(EcsGetWorld(), TestEntity, RENDER_OBJECT_DATA));
+    RdrDestroyModel(ecs_get(EcsGetWorld(), TestEntity, MODEL));
+    RdrDestroyMaterial(&TestMaterial);
+
+    RdrDestroyTexture(GroundTexture);
+    RdrDestroyTexture(Test2Texture);
     RdrDestroyTexture(TestTexture);
 
     EngShutdown();
